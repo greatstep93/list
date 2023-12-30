4fb714e2803c055812572f0fd6ecedef86842f05
kudis <Kudishina1992@bk.ru>
2023-12-30 14:10:28 +0700

    Обёртка

--- src/main/java/ru/greatstep/service/NameService.java
+++ src/main/java/ru/greatstep/service/NameService.java
@@ -1,18 +1,34 @@
 package ru.greatstep.service;
 
+import ru.greatstep.exception.MemoryIsOverException;
+
+import javax.print.attribute.standard.MediaSize;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Spliterator;
 
 public class NameService {
 
     private static final List<String> NAMES = new ArrayList<>();
 
     public static List<String> addName(String name) {
-        return null;
+        if (NAMES.size() == 10) {
+            throw new MemoryIsOverException("Места нет");
+        }
+        if (!NAMES.contains(name.toUpperCase())) {
+            NAMES.add(name.toUpperCase());
+            return NAMES;
+        }
+
+        return NAMES;
     }
 
     public static List<String> getNames() {
         return NAMES;
     }
 
+
 }
+
